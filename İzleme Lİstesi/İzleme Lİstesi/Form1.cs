7524b2492ae9ff8e86c717864516d7623f336038
masamunekuro <99407356+masamunekuro@users.noreply.github.com>
2022-11-25 13:50:53 +0300

    Add files via upload

---
+++ İzleme Lİstesi/İzleme Lİstesi/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace İzleme_Lİstesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        showlar show=new showlar();
+            
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            show.showhazırla();
+            foreach (var item in show.dizi)
+            {
+                listBox1.Items.Add(item);
+            }
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string b;
+            b = textBox1.Text;
+            show.showekle(b);
+            show.showhazırla();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            show.showhazırla();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            show.izlenmedurumu(textBox2.Text);
+            
+        }
+    }
+}
---
+++ İzleme Lİstesi/İzleme Lİstesi/showlar.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace İzleme_Lİstesi
+{
+    class showlar
+    {
+        public string Path { get; set; }
+        
+         
+
+
+       
+
+        public string[] dizi = new string[1];
+        public string[] showhazırla()
+        {
+
+            StreamReader streamread = new StreamReader("C:\\Users\\onur barbaros\\Desktop\\İzleme Lİstesi\\İzleme Lİstesi\\showliste.txt");
+            int i = 0;
+          
+            while (!streamread.EndOfStream)
+            {
+                Array.Resize(ref dizi, i + 1);
+                dizi[i]= streamread.ReadLine() ;
+              
+                i++;
+               
+            }
+            streamread.Close();
+            return dizi;
+            
+
+        }
+       
+        public void showekle(string a)
+        {
+            
+            StreamWriter streamwrite = new StreamWriter("C:\\Users\\onur barbaros\\Desktop\\İzleme Lİstesi\\İzleme Lİstesi\\showliste.txt");
+            foreach (var item in dizi)
+            {
+                streamwrite.WriteLine(item);
+            }
+            streamwrite.WriteLine(a);
+           
+
+            
+           streamwrite.Close();
+           showhazırla();
+        }
+       
+        public int i = 0;
+        public string satır2 = null;
+        public string izlenmedurumu(string a)
+        {
+            StreamReader streamread = new StreamReader("C:\\Users\\onur barbaros\\Desktop\\İzleme Lİstesi\\İzleme Lİstesi\\showliste.txt");
+           
+            string satır = null;
+            
+           
+            string[] öge;
+            while (!streamread.EndOfStream)
+            {
+                satır = streamread.ReadLine();
+               
+                öge = satır.Split('_');
+                if (öge[3]==a)
+                {
+                    if (öge[6] == "izlenmedi")
+                    {
+                        öge[6] = "izlendi";
+
+                        satır = öge[0] + "_" + öge[1] + "_" + öge[2] +"_"+ öge[3]+"_" + öge[4]+"_" + öge[5] + "_" +öge[6];
+
+                        
+                       
+                        
+
+                    }
+                    break;
+                }
+                else
+                {
+                    i++;
+                }
+               
+            }
+
+            streamread.Close();
+           // showhazırla();
+            showyaz(satır);
+            return satır;
+
+        }
+
+        public void showyaz(string satır)
+        {
+            StreamWriter streamwrite = new StreamWriter("C:\\Users\\onur barbaros\\Desktop\\İzleme Lİstesi\\İzleme Lİstesi\\showliste.txt");
+            for (int j = 0; j < dizi.Length; j++)
+            {
+                if (i == j)
+                {
+                    streamwrite.WriteLine(satır);
+                }
+                else
+                {
+                    streamwrite.WriteLine(dizi[j]);
+                }
+            }
+            streamwrite.Close();
+        }
+    }
+
+}
+
